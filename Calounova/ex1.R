f7d6a1f401469b07c311b4bb5822adbc354b1247
CalounovaT <tereza.calounova@gmail.com>
2022-02-24 11:58:45 +0100

    add files

---
+++ Calounova/ex1.R
@@ -0,0 +1,15 @@
+x <- seq(0, 1)
+
+# HUSTOTNI FCE
+curve(dbeta(x,0.5,0.5), col='red')
+curve(dbeta(x,5,1), add = TRUE, col='blue')
+curve(dbeta(x,1,3), add = TRUE, col='green')
+curve(dbeta(x,2,2), add = TRUE, col='purple')
+curve(dbeta(x,2,5), add = TRUE, col='orange')
+
+# DISTRIBUCNI FCE
+curve(pbeta(x,0.5,0.5), col='red')
+curve(pbeta(x,5,1), add = TRUE, col='blue')
+curve(pbeta(x,1,3), add = TRUE, col='green')
+curve(pbeta(x,2,2), add = TRUE, col='purple')
+curve(pbeta(x,2,5), add = TRUE, col='orange')
